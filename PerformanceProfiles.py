2758028176d6cd0b2c06a6085ab4a254489f2bf9
corradocoppola97 <corradocoppola97@gmail.com>
2024-02-11 13:08:03 +0000

    Committ 11feb2024

--- PerformanceProfiles.py
+++ PerformanceProfiles.py
@@ -2,7 +2,7 @@ import os
 import torch
 import matplotlib.pyplot as plt
 dataset_list = [ds[:-4] for ds in os.listdir('dataset')]
-architectures = ['S','M','L','XL','XXL','XXXL']
+architectures = ['S','M','L','XL','XXL','XXXL','4XL']
 all_probs = [(ds,net) for ds in dataset_list for net in architectures]
 os.chdir('prove_reg')
 algos = ['ig','cma','nmcma','lbfgs']
@@ -54,11 +54,15 @@ def r_sp(problem,tau,seed,solver):
 
     return c_ref/min(list_c)
 
-def rho(solver,alpha,tau,seed,big_flag=False):
+def rho(solver,alpha,tau,seed,big_flag=False,small_flag=False):
     rh = 0
+    assert small_flag != big_flag
     if big_flag == True:
         probs = [p for p in all_probs if p[0] in ['BlogFeedback','Covtype','Protein','Skin NonSkin','YearPredictionMSD']
                  and p[1] in ['XXXL','4XL','XXL']]
+    elif small_flag == True:
+        probs = [p for p in all_probs if p[0] not in ['BlogFeedback', 'Covtype', 'Protein', 'Skin NonSkin', 'YearPredictionMSD']
+                 and p[1] not in ['XXXL', '4XL', 'XXL']]
     else:
         probs = all_probs
     for problem in probs:
@@ -68,31 +72,37 @@ def rho(solver,alpha,tau,seed,big_flag=False):
     return rh/len(probs)
 
 
+def plot_PP(big_flag,small_flag,tau):
+    plt.figure()
+    plt.xscale('log')
+    alphas = [1,2,4,8,16,32,64]
+    seeds = [10,100,1000,10000]
+    colors = ['red','blue','green','orange']
+    mk = ['.','o','v','*']
+    idx = 0
+    for solver in algos:
+        pp = []
+        for alpha in alphas:
+            r = sum(rho(solver,alpha,tau,seed,big_flag,small_flag) for seed in seeds)/len(seeds)
+            pp.append(r)
+        plt.plot([_ for _ in alphas],pp,color=colors[idx],linestyle='solid',marker=mk[idx],markersize=7)
+        idx +=1
 
-plt.figure()
-plt.xscale('log')
-alphas = [1,2,4,8,16,32,64]
-seeds = [1]#,10,100,1000,10000]
-tau = 1e-1
-colors = ['red','blue','green','orange']
-mk = ['.','o','v','*']
-idx = 0
-for solver in algos:
-    pp = []
-    for alpha in range(1,65):
-        r = sum(rho(solver,alpha,tau,seed,True) for seed in seeds)/len(seeds)
-        pp.append(r)
-    plt.plot([_ for _ in range(1,65)],pp,color=colors[idx],linestyle='solid',marker=mk[idx],markersize=7)
-    idx +=1
-
-plt.xticks(ticks=alphas,labels=[str(_) for _ in alphas])
-plt.xlabel(chr(945))
-plt.ylabel(chr(961) + '(' + chr(945) + ')')
-plt.legend(algos)
-plt.title(chr(964)+'='+str(tau)+' ')
-plt.show()
+    plt.xticks(ticks=alphas,labels=[str(_) for _ in alphas])
+    plt.xlabel(chr(945))
+    plt.ylabel(chr(961) + '(' + chr(945) + ')')
+    plt.legend(algos)
+    title = chr(964)+'='+str(tau)+' '
+    if big_flag == True: title += '(Big data sets)'
+    if small_flag == True: title += '(Small data sets)'
+    plt.title(title)
+    plt.savefig('PP_'+title+'.pdf')
+    print('DONE')
+    #plt.show()
 
 
-
-
+if __name__ == '__main__':
+    for tau in [1e-1,1e-2,1e-4]:
+        plot_PP(big_flag=True,small_flag=False,tau=tau)
+        plot_PP(big_flag=False, small_flag=True, tau=tau)
 
